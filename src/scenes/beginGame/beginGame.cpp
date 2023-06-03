25498b2b6ef34d47ed94327205ce5735fa796811
flynth <flynth.dev@gmail.com>
2023-06-03 19:33:53 -0400

    Include Factory & Draw rectangle

--- src/scenes/beginGame/beginGame.cpp
+++ src/scenes/beginGame/beginGame.cpp
@@ -1,10 +1,17 @@
 #include "beginGame.hpp"
 
-void BeginGame::Init(){};
+void BeginGame::Init(){
+
+
+    this->rectangle=StartButton();
+
+};
 void BeginGame::Update(){};
 void BeginGame::Render(){
-    glClearColor(0.0f, 0.0f, 0.5f, 1.0f);
+
     glClear(GL_COLOR_BUFFER_BIT);
+    glClearColor(0.1f, 0.0f, 0.0f,1.0f);
+    this->rectangle.Render();
 };
 
 void BeginGame::setDeltaTime(double *deltaTime){
--- src/scenes/beginGame/beginGame.hpp
+++ src/scenes/beginGame/beginGame.hpp
@@ -1,12 +1,15 @@
 #pragma once
 #include "../stage.hpp"
-
+#include "../../utils/classes/Draw/rectangle/rectangle.hpp"
+#include "../../factory/objets/objets.hpp"
 class BeginGame:public Stage{
     private:
         double *deltaTime;
+        RectangleDraw rectangle;
     public:
         void Init()   override;
         void Update() override;
         void Render() override;
         void setDeltaTime(double *deltaTime) override;
+        
 };
