71d0d56bc5ff032f855bec7b72d57f93746f76a7
flynth <flynth.dev@gmail.com>
2023-06-04 18:35:37 -0400

    Include set {mouse,keys}

--- src/scenes/orchestrator.cpp
+++ src/scenes/orchestrator.cpp
@@ -3,7 +3,11 @@
 void Orchestrator::Init(){
     typeStage = Stages::beginGame;
     stage = new BeginGame();
+    stage->setMouse(mouse);
+    stage->setKeys(keys);
+    stage->setMouseKeys(mouseKeys);
     stage->Init();
+
 }
 
 void Orchestrator::Update(){
@@ -18,3 +22,12 @@ void Orchestrator::setDeltaTime(double *deltaTime){
     this->deltaTime=deltaTime;
     stage->setDeltaTime(deltaTime);
 }
+void Orchestrator::setMouse(Mouse *mouse){
+    this->mouse=mouse;
+};
+void Orchestrator::setKeys(std::unordered_map<int,ButtonKey>*Keys){
+    this->keys=Keys;
+};
+void Orchestrator::setMouseKeys(std::unordered_map<int,ButtonKey>*mouseKeys){
+    this->mouseKeys=mouseKeys;
+};
--- src/scenes/stage.hpp
+++ src/scenes/stage.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <GL/glew.h>
+#include "../event/events.hpp"
 
 enum class Stages{
     beginGame,
@@ -13,4 +14,7 @@ class Stage{
         virtual void Render(){};
         virtual Stages getStage(){return Stages::beginGame;};
         virtual void setDeltaTime(double *deltaTime){};
+        virtual void setMouse(Mouse *mouse){};
+        virtual void setKeys(std::unordered_map<int,ButtonKey>*Keys){};
+        virtual void setMouseKeys(std::unordered_map<int,ButtonKey>*mouseKeys){};
 };
