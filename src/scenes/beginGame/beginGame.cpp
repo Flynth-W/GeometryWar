7666ee0e8366a3e25253e1b10e9366b84a12907a
flynth <flynth.dev@gmail.com>
2023-06-04 20:35:12 -0400

    Include in beginStage
        set{mouse,keys}
        start game button

--- src/scenes/beginGame/beginGame.cpp
+++ src/scenes/beginGame/beginGame.cpp
@@ -4,9 +4,20 @@ void BeginGame::Init(){
 
 
     this->rectangle=StartButton();
-
+    this->stage=Stages::beginGame;
 };
-void BeginGame::Update(){};
+void BeginGame::Update(){
+    if( Event::getIfStateKey(keys, GLFW_KEY_ENTER,ButtonState::Released ) ){
+        this->stage=Stages::one;
+    }
+    bool clik=false;
+    if( Event::getIfStateKey(mouseKeys, GLFW_MOUSE_BUTTON_LEFT,ButtonState::Pressed ) ){
+       clik=true; 
+    }
+    if( clik && rectangle.pointIn(mouse->x, mouse->y)  ){
+        this->stage=Stages::one;
+    }
+};
 void BeginGame::Render(){
 
     glClear(GL_COLOR_BUFFER_BIT);
@@ -16,4 +27,17 @@ void BeginGame::Render(){
 
 void BeginGame::setDeltaTime(double *deltaTime){
     this->deltaTime=deltaTime;
+}
+void BeginGame::setMouse(Mouse *mouse){
+    this->mouse=mouse;
+};
+void BeginGame::setKeys(std::unordered_map<int,ButtonKey>*Keys){
+    this->keys=Keys;
+};
+void BeginGame::setMouseKeys(std::unordered_map<int,ButtonKey>*mouseKeys){
+    this->mouseKeys=mouseKeys;
+};
+
+Stages BeginGame::getStage(){
+    return this->stage ;
 }
--- src/scenes/beginGame/beginGame.hpp
+++ src/scenes/beginGame/beginGame.hpp
@@ -2,14 +2,23 @@
 #include "../stage.hpp"
 #include "../../utils/classes/Draw/rectangle/rectangle.hpp"
 #include "../../factory/objets/objets.hpp"
+
 class BeginGame:public Stage{
     private:
         double *deltaTime;
         RectangleDraw rectangle;
+        std::unordered_map<int,ButtonKey>*keys;
+        std::unordered_map<int,ButtonKey>*mouseKeys;
+        Mouse *mouse;
+        Stages stage;
     public:
         void Init()   override;
         void Update() override;
         void Render() override;
         void setDeltaTime(double *deltaTime) override;
+        void setMouse(Mouse *mouse) override;
+        void setKeys(std::unordered_map<int,ButtonKey>*Keys) override;
+        void setMouseKeys(std::unordered_map<int,ButtonKey>*mouseKeys) override;
+        Stages getStage()override;
         
 };
