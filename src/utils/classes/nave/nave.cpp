c4626483d59b16d442441acf027261f5d3588c09
flynth <flynth.dev@gmail.com>
2023-06-06 23:41:08 -0400

    modified Nave
        + type -> Nave
        + Draw Triangle: Values bound by pointers
        + SysBullets: Implemented and done
        +

--- src/utils/classes/nave/nave.cpp
+++ src/utils/classes/nave/nave.cpp
@@ -1,6 +1,7 @@
 #include "nave.hpp"
 
 void Nave::Init(){
+    this->type=TypeObjet::Nave;
     Shader shadera("./shader/square.vs" ,"./shader/square.fs");
     triangle.setShader(shadera);
     float vertex[] = {
@@ -12,29 +13,36 @@ void Nave::Init(){
     };
     triangle.setVetices(vertex,sizeof(vertex));
     this->radio=0.04;
-    this->position= glm::vec3(-0.8);
-    triangle.setPosition(&this->position);
+    this->position=new  glm::vec3(-0.8);
+    triangle.setPosition(this->position);
+    triangle.setAngle(&this->angle);
     this->run=true;
     Event::setKey(keys, GLFW_KEY_J);
     Event::setKey(keys, GLFW_KEY_L);
     Event::setKey(keys, GLFW_KEY_I);
+   
+    bullets.setDeltaTime(this->deltaTime);
+    bullets.setCollisionHandler(this->handleCollision);
+    bullets.Init();
+
 }
 void Nave::Update(){
+    bullets.Update();
     float translationSpeed = 0.9;
     float rotationSpeed = 1.0;
     float limit= 1.0 - this->radio - 0.03;
-    
-    if(Event::getIfStateKey(keys,GLFW_KEY_W, ButtonState::Pressed , ButtonState::Repeat ) &&  limit > position.y){
+    //std::cout << this->position.x <<"  " << this->position.y << std::endl;
+    if(Event::getIfStateKey(keys,GLFW_KEY_W, ButtonState::Pressed , ButtonState::Repeat ) &&  limit > position->y){
         this->triangle.move(0.0f, float( translationSpeed * *deltaTime ) );
     }
-    if(Event::getIfStateKey(keys,GLFW_KEY_S, ButtonState::Pressed , ButtonState::Repeat )&& -limit < position.y){
+    if(Event::getIfStateKey(keys,GLFW_KEY_S, ButtonState::Pressed , ButtonState::Repeat )&& -limit < position->y){
         this->triangle.move(0.0f, float( - translationSpeed * *deltaTime ) );
     }
 
-    if(Event::getIfStateKey(keys,GLFW_KEY_A, ButtonState::Pressed , ButtonState::Repeat ) && -limit < position.x){
+    if(Event::getIfStateKey(keys,GLFW_KEY_A, ButtonState::Pressed , ButtonState::Repeat ) && -limit < position->x){
         this->triangle.move(float( -translationSpeed * *deltaTime ) , 0.0f);
     }
-    if(Event::getIfStateKey(keys,GLFW_KEY_D, ButtonState::Pressed , ButtonState::Repeat ) && limit > position.x){
+    if(Event::getIfStateKey(keys,GLFW_KEY_D, ButtonState::Pressed , ButtonState::Repeat ) && limit > position->x){
         this->triangle.move(float( translationSpeed * *deltaTime ) , 0.0f);
     }
 
@@ -44,9 +52,16 @@ void Nave::Update(){
     if(Event::getIfStateKey(keys,GLFW_KEY_L, ButtonState::Pressed , ButtonState::Repeat )){
         this->triangle.rotate(float( -rotationSpeed * *deltaTime ));
     }
+    if(Event::getIfStateKey(keys,GLFW_KEY_I, ButtonState::Released )){
+        //bullet.setDeltaTime(deltaTime);
+        //bullet.Init( *this->position , this->angle );
+        //handleCollision->add(&bullet);
+        bullets.AddBullet(*this->position, this->angle);
+    }
 }
 void Nave::Render(){
     triangle.Render();
+    bullets.Render();
 }
 void Nave::setDeltaTime(double *deltaTime){
     this->deltaTime = deltaTime;
@@ -59,15 +74,18 @@ bool Nave::getRun(){
 }
 
 glm::vec3 Nave::getPosition(){
-    return this->position;
+    return *this->position;
 }
 double Nave::getRadio(){
     return this->radio;
 }
 void Nave::colision(Iobjet_colicion *a){
-    std::cout << "nave" << std::endl;
+    //std::cout << "nave" << std::endl;
     if(a->type == TypeObjet::Square){
         this->run=false;
     }
 }
 
+void Nave::setCollisionHandler(CollisionsHandler *handleCollision){
+    this->handleCollision=handleCollision;
+}
--- src/utils/classes/nave/nave.hpp
+++ src/utils/classes/nave/nave.hpp
@@ -3,6 +3,7 @@
 #include "../Draw/triangle/triangle.hpp"
 #include "../../../event/events.hpp"
 #include "../collision.hpp"
+#include "../sysBullets/sysBullet.hpp"
 
 class Nave:public Iobjet_colicion{
     private:
@@ -10,14 +11,19 @@ class Nave:public Iobjet_colicion{
         double radio;
         double * deltaTime;
         std::unordered_map<int,ButtonKey>*keys;
-        bool run;
+        bool run=false;
+        //Bullet bullet;
+        SysBullet bullets;
+        float angle;
+        CollisionsHandler *handleCollision;
     public:
         void Init();
         void Update();
         void Render();
+        void setCollisionHandler(CollisionsHandler *handleCollision);
         void setDeltaTime(double *deltaTime);
         void setKeys(std::unordered_map<int,ButtonKey>*Keys) ;
-        bool getRun();
+        bool getRun() override;
 
         glm::vec3 getPosition() override;
         double getRadio() override;
