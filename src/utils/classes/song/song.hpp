e5ceac654a45a2f68821dd10db90be93f000cf6d
flynth <flynth.dev@gmail.com>
2023-06-07 15:31:56 -0400

    Include Audio
        + include lib irrKlang
        + class Song
        + include songs

---
+++ src/utils/classes/song/song.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include "../../../../build/irrKlang/include/irrKlang.h"
+using namespace irrklang;
+
+class Song{
+    private:
+        ISoundEngine *engine;
+    public:
+        Song(const char *file, bool loop);
+        void play();
+};
